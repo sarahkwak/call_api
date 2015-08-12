429fbbe27c4a510967cf3c33adcba75809a776ba
Sarah Kwak <sarahkwak@hotmail.com>
2015-08-12 11:04:33 -0700

    test with ruby calls_controller

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,101 +1,99 @@
-# class CallsController < ActionController::Base
-#   class RestAPI
-#     attr_accessor :auth_id, :auth_token, :url, :version, :api, :headers, :rest
+class CallsController < ActionController::Base
+  class RestAPI
+    attr_accessor :auth_id, :auth_token, :url, :version, :api, :headers, :rest
 
-#     def initialize(url="https://s3.amazonaws.com/plivosamplexml/conference_url.xml", version="v1")
-#         @auth_id = ENV['AUTH_ID']
-#         @auth_token = ENV['AUTH_TOKEN']
-#         @url = url.chomp('/')
-#         @version = version
-#         @api = @url + '/' + @version + '/Account/' + @auth_id
-#         @headers = {"User-Agent" => "RubyPlivo"}
-#         @rest = RestClient::Resource.new(@api, @auth_id, @auth_token)
-#     end
+    def initialize(url="https://s3.amazonaws.com/plivosamplexml/conference_url.xml", version="v1")
+        @auth_id = ENV['AUTH_ID']
+        @auth_token = ENV['AUTH_TOKEN']
+        @url = url.chomp('/')
+        @version = version
+        @api = @url + '/' + @version + '/Account/' + @auth_id
+        @headers = {"User-Agent" => "RubyPlivo"}
+        @rest = RestClient::Resource.new(@api, @auth_id, @auth_token)
+    end
 
-#     def hash_to_params(myhash)
-#         return myhash.map { |k, v| "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}" }.join("&")
-#     end
+    def hash_to_params(myhash)
+        return myhash.map { |k, v| "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}" }.join("&")
+    end
 
-#     def request(method, path, params=nil)
-#         if method == "POST"
-#             if not params
-#                 params = {}
-#             end
-#             begin
-#                 r = @rest[path].post params.to_json, :content_type => 'application/json'
-#             rescue => e
-#                 response = e
-#             end
-#             if not response
-#                 code = r.code
-#                 raw = r.to_str
-#                 response = JSON.parse(raw)
-#             else
-#                 code = response.http_code
-#                 response = JSON.parse(response.response.to_s)
-#             end
-#             return [code, response]
-#         elsif method == "GET"
-#             if params
-#                 path = path + '?' + hash_to_params(params)
-#             end
+    def request(method, path, params=nil)
+        if method == "POST"
+            if not params
+                params = {}
+            end
+            begin
+                r = @rest[path].post params.to_json, :content_type => 'application/json'
+            rescue => e
+                response = e
+            end
+            if not response
+                code = r.code
+                raw = r.to_str
+                response = JSON.parse(raw)
+            else
+                code = response.http_code
+                response = JSON.parse(response.response.to_s)
+            end
+            return [code, response]
+        elsif method == "GET"
+            if params
+                path = path + '?' + hash_to_params(params)
+            end
 
-#             begin
-#                 r = @rest[path].get
-#             rescue => e
-#                 response = e
-#             end
-#             if not response
-#                 code = r.code
-#                 raw = r.to_str
-#                 response = JSON.parse(raw)
-#             else
-#                 code = response.http_code
-#                 response = JSON.parse(response.response.to_s)
-#             end
-#             return [code, response]
-#         elsif method == "DELETE"
-#             if params
-#                 path = path + '?' + hash_to_params(params)
-#             end
-#             begin
-#                 r = @rest[path].delete
-#             rescue => e
-#                 response = e
-#             end
-#             if not response
-#                 code = r.code
-#             else
-#                 code = response.http_code
-#                 response = JSON.parse(response.response.to_s)
-#             end
-#             return [code, ""]
-#         end
-#         return [405, 'Method Not Supported']
-#     end
-#     def make_call(params={})
-#           return request('POST', "/Call/", params)
-#     end
-#   end
-#   AUTH_ID = ENV['AUTH_ID']
-#   AUTH_TOKEN = ENV['AUTH_TOKEN']
-#   def makecall
-#     p = RestAPI.new(AUTH_ID, AUTH_TOKEN)
-#     params = {
-#         'to' => '16509245485', # The phone number to which the call has to be placed
-#         'from' => '16503819918', # The phone number to be used as the caller id
-#         'answer_url' => 'https://s3.amazonaws.com/static.plivo.com/answer.xml' # The URL invoked by Plivo when the outbound call is answered
-#         # 'answer_method' => 'GET', # The method used to call the answer_url
-#         # Example for Asynchrnous request
-#         #'callback_url' => "https://enigmatic-cove-3140.herokuapp.com/callback", # The URL notified by the API response is available and to which the response is sent.
-#         #'callback_method' => "GET" # The method used to notify the callback_url.
-#     }
+            begin
+                r = @rest[path].get
+            rescue => e
+                response = e
+            end
+            if not response
+                code = r.code
+                raw = r.to_str
+                response = JSON.parse(raw)
+            else
+                code = response.http_code
+                response = JSON.parse(response.response.to_s)
+            end
+            return [code, response]
+        elsif method == "DELETE"
+            if params
+                path = path + '?' + hash_to_params(params)
+            end
+            begin
+                r = @rest[path].delete
+            rescue => e
+                response = e
+            end
+            if not response
+                code = r.code
+            else
+                code = response.http_code
+                response = JSON.parse(response.response.to_s)
+            end
+            return [code, ""]
+        end
+        return [405, 'Method Not Supported']
+    end
+    def make_call(params={})
+          return request('POST', "/Call/", params)
+    end
+  end
+  AUTH_ID = ENV['AUTH_ID']
+  AUTH_TOKEN = ENV['AUTH_TOKEN']
+  def makecall
+    p = RestAPI.new(AUTH_ID, AUTH_TOKEN)
+    params = {
+        'to' => '16509245485', # The phone number to which the call has to be placed
+        'from' => '16503819918', # The phone number to be used as the caller id
+        'answer_url' => 'https://s3.amazonaws.com/static.plivo.com/answer.xml' # The URL invoked by Plivo when the outbound call is answered
+        # 'answer_method' => 'GET', # The method used to call the answer_url
+        # Example for Asynchrnous request
+        #'callback_url' => "https://enigmatic-cove-3140.herokuapp.com/callback", # The URL notified by the API response is available and to which the response is sent.
+        #'callback_method' => "GET" # The method used to notify the callback_url.
+    }
 
-#     # Make an outbound call
-#     response = p.make_call(params)
-#     print response
-#   end
-# end
+    # Make an outbound call
+    response = p.make_call(params)
+    print response
+  end
+end
 
-# <%= button_to "call", {:controller => :calls,
-#       :action => 'makecall'} %>
